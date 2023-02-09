f97b1709b914c53a8932b178a760afb18329bce9
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2023-02-09 19:28:04 +0300

    More tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,15 @@
-
-
+#  Copyright (c) 2022 https://reportportal.io .
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#  https://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License
 import logging
 import pytest
 from reportportal_client import RPLogger
@@ -15,3 +25,21 @@ def rp_logger():
     logger.setLevel(logging.DEBUG)
     logging.setLoggerClass(RPLogger)
     return logger
+
+
+@pytest.fixture(scope='session')
+def rp_launch_id(request):
+    if hasattr(request.config, "py_test_service"):
+        return request.config.py_test_service.rp.launch_id
+
+
+@pytest.fixture(scope='session')
+def rp_endpoint(request):
+    if hasattr(request.config, "py_test_service"):
+        return request.config.py_test_service.rp.endpoint
+
+
+@pytest.fixture(scope='session')
+def rp_project(request):
+    if hasattr(request.config, "py_test_service"):
+        return request.config.py_test_service.rp.project
---
+++ tests/test_logging.py
@@ -0,0 +1,36 @@
+#  Copyright (c) 2022 https://reportportal.io .
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#  https://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+def test_report_portal_logging():
+    """An example test with shows how to use standard python logger."""
+    logger.info("Standard logger logs to Report Portal")
+    assert True
+
+
+def test_launch_url_get(rp_logger, rp_launch_id, rp_endpoint, rp_project):
+    """
+    This is a test which gets Launch ID, Report Portal endpoint and Project
+    Name from fixtures in `conftest.py` and prints the result in logs, both: in
+    console and on Report Portal
+    """
+    rp_logger.info("Got launch ID: %s", rp_launch_id)
+    rp_logger.info("Launch URL: %s/ui/#%s/launches/all/%s", rp_endpoint,
+                   rp_project, rp_launch_id)
+    assert True
+
